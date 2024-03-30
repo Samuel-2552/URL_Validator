41681cf45b464315b55543c94df17924db7eceee
Samuel-2552 <201501502@rajalakshmi.edu.in>
2024-03-30 18:23:42 +0530

    trying crawling

--- app.py
+++ app.py
@@ -2,32 +2,53 @@ from flask import Flask, render_template, request, jsonify
 import requests
 from bs4 import BeautifulSoup
 import re
+from urllib.parse import urlparse, urljoin
 
 app = Flask(__name__)
 
 
 def validate_url(url):
     if not re.match(r'http(s)?://', url):
-        url = 'http://' + url
+        url = 'https://' + url
     return url
 
 
-def check_keyword(url, keywords):
+def check_keyword(url, keywords, crawl=False, visited_links=None):
     try:
+        if visited_links is None:
+            visited_links = set()
+
         response = requests.get(url, allow_redirects=True, verify=True, timeout=30, headers={
                                 'User-Agent': 'Mozilla/5.0'}, stream=True)
         status_code = response.status_code
         keyword_found = False
+
         if status_code == 200:
             content = response.text.lower()  # Convert content to lowercase
-            # print("Content for URL", url, ":", content)  # Debug statement
             for keyword in keywords:
                 keyword = keyword.strip().lower()  # Convert keyword to lowercase
-                print("Checking keyword:", keyword)  # Debug statement
                 if keyword in content:
                     keyword_found = True
                     break
+
+            if crawl:
+                soup = BeautifulSoup(content, 'html.parser')
+                links = soup.find_all('a', href=True)
+                for link in links:
+                    href = link['href']
+                    if href.startswith('#'):  # ignore anchor links
+                        continue
+                    full_url = urljoin(url, href)
+                    if full_url not in visited_links:
+                        visited_links.add(full_url)
+                        found = check_keyword(
+                            full_url, keywords, crawl=True, visited_links=visited_links)
+                        if found['keyword_found']:
+                            keyword_found = True
+                            break
+
         return {'status_code': status_code, 'keyword_found': keyword_found}
+
     except requests.exceptions.RequestException as e:
         return {'status_code': str(e), 'keyword_found': False}
 
